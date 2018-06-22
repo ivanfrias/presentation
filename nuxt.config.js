a1e2c6836ab89c1eb93ba63e0146f9554c14722a
Ivan Frias <frias.ivan@gmail.com>
2018-06-22 23:21:02 +0100

    some shit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,13 @@
-module.exports = {
+/* nuxt.config.js */
+// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
   router: {
-    base: '/work-in-progress'
-  },
+    base: '/work-in-progress/'
+  }
+} : {}
+
+module.exports = {
+  routerBase,
   /*
   ** Headers of the page
   */
